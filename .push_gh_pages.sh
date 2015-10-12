dcd18a097f51d8d3ee37c4c69a212861c1790002
Markus Gesmann <markus.gesmann@gmail.com>
2015-10-12 20:03:49 +0100

    Test epub publishing

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -11,11 +11,17 @@ for files in '*.tar.gz'; do
 tar xfz $files
 done
 
+cd ./PSRWP/inst/doc
+R -e 'library(knitr);knit("PSRWP-HTML.Rmd", "PSRWP-EPUB.md" )'
+pandoc  PSRWP-EPUB.md --to epub3 --from markdown --output PSRWP-EPUB.epub --bibliography PSRWP.bib --number-sections --highlight-style tango --chapters --filter pandoc-citeproc --table-of-contents --toc-depth 2
+cd ../../../
+
 cd out
 git init
 git config user.name "mages-travis"
 git config user.email "travis"
 cp ../PSRWP/inst/doc/* .
+
 mv PSRWP.html index.html
 
 git add .
