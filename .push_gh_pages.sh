23ab6dd97872504c668591ddec6e4518feff6fb8
Markus Gesmann <markus.gesmann@gmail.com>
2015-10-13 07:46:10 +0100

    Roll back

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install jq to filter Github release data for Pandoc.
-sudo apt-get -y install jq
-# Get the latest .deb released.
-wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
-sudo dpkg -i pandoc.deb
-
 rm -rf out || exit 0;
 mkdir out;
 
@@ -17,18 +11,15 @@ for files in '*.tar.gz'; do
 tar xfz $files
 done
 
-#cd ./PSRWP/inst/doc
-#R -e 'library(rmarkdown);render("PSRWP-Word.Rmd" )'
-#cd ../../../
-
 cd out
 git init
 git config user.name "mages-travis"
 git config user.email "travis"
 cp ../PSRWP/inst/doc/* .
-
 mv PSRWP.html index.html
 
 git add .
 git commit -m "deployed to github pages"
 git push --force --quiet $FULL_REPO master:gh-pages
+
+
