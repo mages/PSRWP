fc87b8c6ad6f53f204791a1e2f1dd8bbcd36a089
mages-travis <travis>
2016-04-21 08:36:01 +0000

    deployed to github pages

---
+++ IFoA_PSRWP-HTML.R
@@ -0,0 +1,135 @@
+## ----setup, echo=FALSE, message=FALSE, warning=FALSE---------------------
+library("knitcitations")
+cleanbib()
+options("citation_format" = "pandoc") 
+bib <- read.bibtex("../Bibilography/PSRWP.bib")
+library(captioner)
+table_nums <- captioner(prefix = "Table")
+fig_nums <- captioner()
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:UltimateView", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:OneYearView", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AdditiveScaling", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:MultiplicativeScaling", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ParameterError", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ProcessError", display=FALSE) 
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ModelError", display=FALSE) 
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ResidualScatterSim1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ResidualScatterSim2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:CorrectModelFitted", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:UnmodelledPaymentTrend", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AccidentDevelopment1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AccidentPayment1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AccidentDevelopment2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AccidentPayment2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Plot_with_correctly_specified_distribution", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Plot_with_incorrectly_specified_distribution", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ExampleDataSet1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Chain-ladderpaymentprojectionsforexampledataset1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Chain-ladderreserveestimatesforexampledataset1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:LogMacksigma2Parameters", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ODPdiagnosticplots", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Actualfittedheatmapsfordataset1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:P-PplotfortheODPmodel", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:BootstrappingresultsfortheODPmodel", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:SourcesoferrorfortheODPbootsrappingresults", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Distributionoftheoutstandindclaimsreserves", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AnalyticresultsforMacksmodel", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ComparisonofreserveCoV", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:BFreserveestimatesfordataset1", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AnalyticresultsfortheBFmodel", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ComparisonofBFandODPRMSEP", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Exampledataset2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ResidualScatterSim2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Chain-ladderreserveestimatesforexampledataset2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:LogMacksigma2parameters2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:ODPdiagnosticplots2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:Actualfittedheatmapsfordataset2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:MackandODPcoefficientsofvariationfordataset2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:BFreserveestimatesfordataset2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:AnalyticresultsfortheBFmodel2", display=FALSE)
+
+## ----echo=FALSE----------------------------------------------------------
+fig_nums(name = "fig:SourceoferrorfortheBFanalyticresults", display=FALSE)
+
